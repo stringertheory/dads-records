c17f7129307a54fb6f2a2fe5f8a8aa1d6d5cf8a8
stringertheory <mike.stringer.internet@gmail.com>
2024-09-14 21:38:19 -0500

    Refactor requests handling and caching - add_to_discogs.py: Improved session handling with requests_cache and added sleep function for rate limiting.

--- add_to_discogs.py
+++ add_to_discogs.py
@@ -3,20 +3,30 @@ import csv
 import os
 import requests
 import requests_cache
+import time
 
 username = "tomstringer"
-folder_id = "7751405" # BeepBopBoop
+folder_id = "7751405"  # BeepBopBoop
 token = os.getenv("DISCOGS_TOKEN_DAD")
 api_url = "https://api.discogs.com"
 
-def main():
 
-    requests_cache.install_cache()
-    
+def sleep(response):
+    if hasattr(response, "from_cache") and response.from_cache:
+        time.sleep(0.5 + random.random())
+
+
+def main():
+    session = requests_cache.CachedSession(
+        "discogs", allowable_methods=("GET", "POST")
+    )
+
     filename = sys.argv[1]
     with open(filename) as infile:
         reader = csv.DictReader(infile)
-        data = [row for row in reader if row.get('artist') and row.get('album')]
+        data = [
+            row for row in reader if row.get("artist") and row.get("album")
+        ]
 
     for row in data:
         query_params = {
@@ -24,19 +34,18 @@ def main():
             "type": "release",
             "token": token,
         }
-        print(query_params)
 
         url = f"{api_url}/database/search"
-        response = requests.get(url, params=query_params)
+        response = session.get(url, params=query_params)
+        sleep(response)
         data = response.json()
         release_id = data["results"][0]["id"]
-        breakpoint()
 
         url = f"{api_url}/users/{username}/collection/folders/{folder_id}/releases/{release_id}"
-        print(url)
 
-        response = requests.post(url, params={"token": token})
-        breakpoint()
+        response = session.post(url, params={"token": token})
+        sleep(response)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
