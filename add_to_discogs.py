dac861d6b3baa3c70dd0c3936b5f0cbf6c68c411
stringertheory <mike.stringer.internet@gmail.com>
2024-09-14 23:25:52 -0500

    Enhance discogs interaction functionality - add_to_discogs.py: Added functionality to include batch number in Discogs fields. - add_to_discogs.py: improved caching - add_to_discogs.py: folder instead of csv name

--- add_to_discogs.py
+++ add_to_discogs.py
@@ -1,41 +1,70 @@
-import sys
 import csv
+import json
 import os
+import random
+import sys
+import time
+
 import requests
 import requests_cache
-import time
 
 username = "tomstringer"
 folder_id = "7751405"  # BeepBopBoop
 token = os.getenv("DISCOGS_TOKEN_DAD")
 api_url = "https://api.discogs.com"
+batch_field_id = 4
 
 
 def sleep(response):
-    if hasattr(response, "from_cache") and response.from_cache:
-        time.sleep(0.5 + random.random())
+    if hasattr(response, "from_cache") and not response.from_cache:
+        delay = 0.5 + random.random()
+        print(f"sleep for {delay:.1f}")
+        time.sleep(delay)
+    else:
+        print("cached")
+
+
+def get_filenames():
+    batch_directory = sys.argv[1]
+    if not os.path.isdir(batch_directory):
+        raise ValueError(f"{batch_directory} is not a directory")
+
+    basename = "artist_album.csv"
+    csv_filename = os.path.join(batch_directory, basename)
+
+    batch_number = batch_directory.strip(" /").split("/")[-1]
+
+    msg = f"found {csv_filename}, batch number {batch_number}"
+    print(msg, file=sys.stderr)
+
+    return csv_filename, batch_number
 
 
 def main():
+    csv_filename, batch_number = get_filenames()
+
     session = requests_cache.CachedSession(
-        "discogs", allowable_methods=("GET", "POST")
+        f"cache/discogs-{batch_number}",
+        allowable_methods=("GET", "POST"),
+        allowable_codes=(200, 201, 202, 203, 204),
     )
 
-    filename = sys.argv[1]
-    with open(filename) as infile:
+    with open(csv_filename) as infile:
         reader = csv.DictReader(infile)
         data = [
             row for row in reader if row.get("artist") and row.get("album")
         ]
 
     for row in data:
+        query = f"{row['artist']} - {row['album']}"
         query_params = {
-            "q": f"{row['artist']} - {row['album']}",
+            "q": query,
             "type": "release",
             "token": token,
         }
 
         url = f"{api_url}/database/search"
+        print(f"making query {query}", file=sys.stderr)
         response = session.get(url, params=query_params)
         sleep(response)
         data = response.json()
@@ -43,7 +72,25 @@ def main():
 
         url = f"{api_url}/users/{username}/collection/folders/{folder_id}/releases/{release_id}"
 
+        print(f"adding release {release_id}", file=sys.stderr)
         response = session.post(url, params={"token": token})
+        sleep(response)
+
+        instance_id = response.json()["instance_id"]
+        url = f"{api_url}/users/{username}/collection/folders/{folder_id}/releases/{release_id}/instances/{instance_id}/fields/{batch_field_id}"
+
+        print(
+            f"adding batch number {batch_number} to {instance_id}",
+            file=sys.stderr,
+        )
+        response = session.post(
+            url,
+            headers={
+                "Content-Type": "application/json",
+            },
+            params={"token": token},
+            data=json.dumps({"value": batch_number}),
+        )
         sleep(response)
 
 
