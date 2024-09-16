37ccb1f4b55fbd9fd7cb90ae650d2c51fb0f37d8
stringertheory <mike.stringer.internet@gmail.com>
2024-09-16 13:19:39 -0500

    just a stub for now

---
+++ double_checker.py
@@ -0,0 +1,11 @@
+# get all releases from collection that are not a verified pressing
+
+# for each release, use the master release ID to get all other
+# releases that share the same master
+
+# check to see if any of the releases are super valuable
+
+from remove_duplicates import get_all_releases
+
+result = get_all_releases()
+breakpoint()
