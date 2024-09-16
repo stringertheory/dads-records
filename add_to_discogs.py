7a4b8facda7a8c868759b812e7c10036b3e629ed
stringertheory <mike.stringer.internet@gmail.com>
2024-09-16 13:17:25 -0500

    no more cacheing

--- add_to_discogs.py
+++ add_to_discogs.py
@@ -6,7 +6,6 @@ import sys
 import time
 
 import requests
-import requests_cache
 
 username = "tomstringer"
 token = os.getenv("DISCOGS_TOKEN_DAD")
@@ -20,7 +19,7 @@ manual_folder_id = "7758341"
 testing_folder_id = "7758632"
 
 def sleep(response):
-    if hasattr(response, "from_cache") and not response.from_cache:
+    if not hasattr(response, "from_cache") or (hasattr(response, "from_cache") and not response.from_cache):
         delay = 0.6 + random.random()
         print(f"sleep for {delay:.1f}")
         time.sleep(delay)
@@ -51,11 +50,7 @@ def get_best_result(data):
 def main():
     csv_filename, batch_number = get_filenames()
 
-    session = requests_cache.CachedSession(
-        f"cache/discogs-{batch_number}",
-        allowable_methods=("GET", "POST"),
-        allowable_codes=(200, 201, 202, 203, 204),
-    )
+    session = requests.Session()
 
     with open(csv_filename) as infile:
         reader = csv.DictReader(infile)
@@ -63,14 +58,22 @@ def main():
             row for row in reader if row.get("artist") and row.get("album")
         ]
 
-    for row in data:
+    for row_number, row in enumerate(data):
 
+        print(f"doing row number {row_number}", file=sys.stderr)
+
+        if row["artist"] in {"Various Artists", "Various", "Unknown"}:
+            row["artist"] = ""
+        
         queries = []
         if row["catalog_number"] and row["record_label"]:
             queries.append((catalog_folder_id, f"{row['artist']} {row['album']} {row['record_label']} {row['catalog_number']}"))
         if row["record_label"]:
             queries.append((label_folder_id, f"{row['artist']} {row['album']} {row['record_label']}"))
         queries.append((name_folder_id, f"{row['artist']} {row['album']}"))
+
+        # for debugging
+        # if row_number > 17: continue
         
         for folder_id, query in queries:
         
@@ -90,7 +93,7 @@ def main():
                 break
 
         if not release_id:
-            print(f"{'*' * 20} didn't find match for {query}", file=sys.stderr)
+            print(f"{'*' * 120}\n\ndidn't find match for {query}\n\n{'*' * 120}", file=sys.stderr)
             continue
 
         # folder_id = testing_folder_id
@@ -118,6 +121,7 @@ def main():
         )
         sleep(response)
 
+        print("", file=sys.stderr)
 
 if __name__ == "__main__":
     main()
