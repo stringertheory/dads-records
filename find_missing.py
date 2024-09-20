fe10d040b78d7091f5a6d7e620431eea972b03b5
stringertheory <mike.stringer.internet@gmail.com>
2024-09-19 23:55:32 -0500

    more strict checking for number in batches

--- find_missing.py
+++ find_missing.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import collections
+import glob
 
 grouped = collections.defaultdict(list)
 with open(sys.argv[1]) as infile:
@@ -9,20 +10,25 @@ with open(sys.argv[1]) as infile:
         grouped[int(row["Collection Batch"])].append(row)
 
 for batch, row_list in sorted(grouped.items()):
-    print(batch, len(row_list))
+    with open(f"records/{batch}/artist_album.csv") as infile:
+        reader = csv.DictReader(infile)
+        rows = [row for row in reader]
+    pix = glob.glob(f"records/{batch}/*.jpg")
+    print(batch, len(row_list), len(rows), len(pix))
+    assert len(row_list) == len(rows) == len(pix)
     # if batch == 107:
     #     for row in sorted(row_list, key=lambda i: i["Artist"]):
     #         print(row["Artist"], row["Title"])
 
-batch = 107
-with open("col", "w") as outfile:
-    for row in sorted(grouped[107], key=lambda i: (i["Artist"].lower(), i["Title"].lower())):
-        print(row["Artist"], row["Title"], file=outfile)
+# batch = 107
+# with open("col", "w") as outfile:
+#     for row in sorted(grouped[107], key=lambda i: (i["Artist"].lower(), i["Title"].lower())):
+#         print(row["Artist"], row["Title"], file=outfile)
 
-with open(f"records/{batch}/artist_album.csv") as infile, open("fol", "w") as outfile:
-    reader = csv.DictReader(infile)
-    for row in sorted(reader, key=lambda i: (i['artist'].lower(), i['album'].lower())):
-        print(row['artist'], row['album'], file=outfile)
+# with open(f"records/{batch}/artist_album.csv") as infile, open("fol", "w") as outfile:
+#     reader = csv.DictReader(infile)
+#     for row in sorted(reader, key=lambda i: (i['artist'].lower(), i['album'].lower())):
+#         print(row['artist'], row['album'], file=outfile)
 
 
     
