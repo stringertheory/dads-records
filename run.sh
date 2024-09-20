2e1528987463641306673514f98167294ecbf09f
stringertheory <mike.stringer.internet@gmail.com>
2024-09-19 23:56:14 -0500

    adding batch page script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python get_album_image_info.py "$@" &&
 python make_index.py &&
-rsync -cvzr records/ norbert:/var/www/files/records/ &&
-python add_to_discogs.py "$@"
+python make_batch_pages.py &&
+rsync -cvzr records/ norbert:/var/www/files/records/
+# python add_to_discogs.py "$@"
