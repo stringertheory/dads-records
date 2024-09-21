96dc01b84de9b1995c1f3d3bcc72a3da0a416592
stringertheory <mike.stringer.internet@gmail.com>
2024-09-21 13:47:50 -0500

    adding in verification step

--- add_to_discogs.py
+++ add_to_discogs.py
@@ -1,9 +1,11 @@
+import webbrowser
 import csv
 import json
 import os
 import random
 import sys
 import time
+import urllib
 
 import requests
 
@@ -11,6 +13,7 @@ username = "tomstringer"
 token = os.getenv("DISCOGS_TOKEN_DAD")
 api_url = "https://api.discogs.com"
 batch_field_id = 4
+verified_field_id = 5
 beep_bop_boop_id = "7751405"
 catalog_folder_id = "7758311"
 label_folder_id = "7758317"
@@ -96,7 +99,55 @@ def main():
             print(f"{'*' * 120}\n\ndidn't find match for {query}\n\n{'*' * 120}", file=sys.stderr)
             with open('no-matches.txt', 'a') as outfile:
                 print(query + f" (batch number {batch_number})", file=outfile)
+            encoded = urllib.parse.urlencode({"q": query, "type": "release"})
+            url = "https://www.discogs.com/search/?" + encoded
+        else:
+            url = f'https://www.discogs.com{response.json()["results"][0]["uri"]}'
+
+        webbrowser.open(url)
+            
+        # open webbrowser to release page
+        if folder_id == catalog_folder_id:
+            match_type = "Catalog"
+        elif folder_id == label_folder_id:
+            match_type = "Label"
+        elif folder_id == name_folder_id and release_id:
+            match_type = "Name"
+        else:
+            match_type = "No"
+
+        msg = f"{match_type} Match, release_id={release_id}. What to do?\n"
+        msg += "    (a) use this release id and mark as verified\n"
+        msg += "    (b) use this release id but don't mark as verified\n"
+        msg += "    (m) manually enter a release id\n"
+        msg += "    (skip) skip this record (nothing will be entered in catalog)\n"
+        valid_responses = ['a', 'b', 'm', 'skip']
+        while True:
+            input_response = input(msg)
+            if input_response.lower() in valid_responses:
+                break
+            print(f"{input_response!r} is an invalid response, must be in {valid_responses!r}")
+
+        if input_response == 'a':
+            verified = True
+        elif input_response == 'b':
+            verified = False
+        elif input_response == 'm':
+            verified = True
+            folder_id = manual_folder_id
+            while True:
+                input_response = input("Enter release ID: ")
+                try:
+                    release_id = int(input_response)
+                except ValueError:
+                    print("invalid release id")
+                    continue
+                else:
+                    break
+        elif input_response == 'skip':
             continue
+        else:
+            raise Exception("This should be impossible. You have a bug.")
 
         # folder_id = testing_folder_id
         
@@ -123,6 +174,22 @@ def main():
         )
         sleep(response)
 
+        if verified:
+            print(
+                f"adding verified to {instance_id}",
+                file=sys.stderr,
+            )
+            url = f"{api_url}/users/{username}/collection/folders/{folder_id}/releases/{release_id}/instances/{instance_id}/fields/{verified_field_id}"
+            response = session.post(
+                url,
+                headers={
+                    "Content-Type": "application/json",
+                },
+                params={"token": token},
+                data=json.dumps({"value": "yes"}),
+            )
+            sleep(response)
+        
         print("", file=sys.stderr)
 
 if __name__ == "__main__":
